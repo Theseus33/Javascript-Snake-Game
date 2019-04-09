7faa1ba02438c6e55df6e92681cda82e366cef20
Rana Bhattacharyya <ranab33@gmail.com>
2019-04-09 16:30:08 -0400

    variables added before being drawn to canvas

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ ground.src = "img/ground.png";
 
 const food = new Image();
 ground.src = "img/food.png";
+
+//create the snake as an array
+
+let snake = [];
+snake[0] = {
+  x: 9 * box,
+  y: 10 * box
+};
+
+//food made randomized using x and y positions
+
+let food = {
+  x: Math.floor(Math.random() * 17 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 3) * box
+};
+
+//score variable
+
+let score = 0;
